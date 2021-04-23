515c8966cdbae9736bd08775d3bb874776dd9168
ruddy palvair <r.palvair@gmail.com>
2021-04-23 11:49:26 +0200

    feat : Introduce header component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <template>
+  <the-header></the-header>
   <router-view></router-view>
 </template>
 
 <script>
+import TheHeader from "./components/TheHeader.vue";
 export default {
   name: "App",
+  components: { TheHeader },
 };
 </script>
 
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,5 @@
+<template>
+    <header>
+        <h3>The header</h3>
+    </header>
+</template>
