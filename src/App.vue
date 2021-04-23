e931a1b18e5b41d02e62ecaa0c2d9fe611d8bd03
ruddy palvair <r.palvair@gmail.com>
2021-04-23 12:23:24 +0200

    feat : Install bootstrap feat : Complete header feat: Add footer

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,17 @@
 <template>
   <the-header></the-header>
   <router-view></router-view>
+  <the-footer></the-footer>
 </template>
 
 <script>
 import TheHeader from "./components/TheHeader.vue";
+import TheFooter from "./components/TheFooter.vue";
 export default {
   name: "App",
-  components: { TheHeader },
+  components: { TheHeader, TheFooter },
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,5 +1,24 @@
 <template>
-    <header>
-        <h3>The header</h3>
-    </header>
+  <nav class="navbar navbar-light bg-light">
+    <div>
+      <span class="navbar-brand mb-0 h1">My Contacts</span>
+      <span class="techno">with Vue {{ version }}</span>
+    </div>
+  </nav>
 </template>
+<script>
+import packageJson from "../../package.json";
+export default {
+  data() {
+    return {
+      version: packageJson.dependencies.vue.substring(1),
+    };
+  },
+};
+</script>
+<style scoped>
+.navbar.navbar-light.bg-light {
+  padding-top: 20px;
+  padding-bottom: 20px;
+}
+</style>
